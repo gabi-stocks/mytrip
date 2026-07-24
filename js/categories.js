72a832d52f825eac18d92d2389fd54670e6f7ee6
gabi others <gabidorip@gmail.com>
2026-07-24 18:15:49 +0300

    Update categories.js

--- js/categories.js
+++ js/categories.js
@@ -1,6 +1,7 @@
 export const CATEGORY_LABELS = {
   lodging: "לינה",
   attraction: "אטרקציה",
+  trip: "טיול",
   restaurant: "מסעדה",
   other: "אחר"
 };
@@ -8,6 +9,7 @@ export const CATEGORY_LABELS = {
 export const CATEGORY_COLORS = {
   lodging: "#C1542E",
   attraction: "#B9800F",
+  trip: "#3B6E91",
   restaurant: "#4C6B4A",
   other: "#6B675C"
 };
